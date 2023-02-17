c37cb8d5e8365997ec0d6a9db4dc0266d08edde2
caren <carenyambura1010@gmail.com>
2023-02-17 07:01:51 -0800

    8 task modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: always 1 (success)
+ * Return: always 1 if no error
  */
 int main(void)
 {
-	written("and that piece of art is useful\", - Dora Korpar, 2015-10-19, 59);
+	written(1, "and that piece of art is useful\", - Dora Korpar, 2015-10-19, 59);
 	return (1);
 }
