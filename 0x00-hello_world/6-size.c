d7da1de75fe64f751cae7ed20ab4d006f8619613
caren <carenyambura1010@gmail.com>
2023-02-17 05:19:48 -0800

    6 task modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
+ *
  * Return: 0 (success)
+ *
  */
 int main(void)
 {
-
 printf("Size of a char: %d byte(s)\n",sizeof(char));
 printf("Size of an int: %d byte(s)\n",sizeof(int));
 printf("Size of a long: %d byte(s)\n",sizeof(long int));
