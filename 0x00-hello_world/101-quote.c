4a59845ec802ece09ecccdcaf94e09a46ad9011b
caren <carenyambura1010@gmail.com>
2023-02-17 07:08:24 -0800

    8 task renew

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: always 1 if no error
+ * Return: always 1 (success)
  */
 int main(void)
 {
-	written(1, "and that piece of art is useful\", - Dora Korpar, 2015-10-19, 59);
+	write(1, "and that piece of art is useful\", - Dora Korpar, 2015-10-19, 59);
 	return (1);
 }
