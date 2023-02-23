91a90febf984c5b14803d81c6d2da4b099c8e28f
caren <carenyambura1010@gmail.com>
2023-02-23 00:50:04 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0  to 9.
+ * Return: no return.
+ */
+{
+	inch ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
