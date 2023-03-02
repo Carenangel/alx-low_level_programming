582390c613f03296f5de48528394f75a7cb19e2c
caren <carenyambura1010@gmail.com>
2023-03-02 11:04:05 -0800

    low level programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Determines if a number is negative, zero or positive.
+ * main - Determines if a number is positive, negative or zero.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
